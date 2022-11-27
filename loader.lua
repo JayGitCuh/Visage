2bd55c0e6e86fc43504f8a287b013990c8642152
Jay <94145603+JayGitCuh@users.noreply.github.com>
2022-11-27 00:15:31 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,17 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/JayGitCuh/Visage/main/Games/Trident.lua")))()
+local Stuff = {
+    github = "https://raw.githubusercontent.com/JayGitCuh/Visage/main/",
+};
+local Games = {
+    ["Universal"] = "Games/Universal",
+    ["9570110925"] = "Games/Trident",
+    ["3233893879"] = "Games/badbusiness"
+};
+local function LoadScript(script)
+   loadstring(game:HttpGet(Stuff["github"]..'/'..script..'.lua', true))()
+end
+
+if Games[tostring(game.PlaceId)] then
+    LoadScript(Games[tostring(game.PlaceId)])
+else
+    LoadScript(Games["Universal"])
+end
